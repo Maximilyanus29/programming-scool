d78c96c7f785d4e3a3f8d3d0176dee37709119dc
Ryabchik <max-r2010@mail.ru>
2022-04-11 00:41:50 +0300

    awfas

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
 import '../scss/main.scss';
 import '../index.html';
-import '../about.html';
+import '../3d-blender.html';
+import '../3d-game-unity.html';
+import '../anime-art.html';
+import '../digital-art.html';
+import '../mathematic.html';
+import '../minecraft.html';
+import '../python.html';
+import '../roblox.html';
+import '../web-design.html';
 
 import mymodal from "./modules/mymodal";
 
